d2636b6bc9d53cc5373b2aec9635890a5cd2a3d4
alandlan <alanhorcrux@gmail.com>
2024-02-20 20:08:50 -0300

    create blank project

---
+++ todo/App.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Home from './src/views/home';
+
+export default function App() {
+  return (
+    <Home />
+  );
+}
+
+
---
+++ todo/src/components/header/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+import styles from "./styles";
+
+export default function Header() {
+  return (
+    <View>
+      <Text>Header</Text>
+    </View>
+  );
+}
